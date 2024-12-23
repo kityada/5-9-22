90b3b5f2ea0e6f1ac76f4a216ce8907431b3099d
kityada <kringman2371@gmail.com>
2024-12-23 14:33:23 +0700

    Add files via upload

---
+++ ข้อ3..py
@@ -0,0 +1,11 @@
+width = float(input())
+length = float(input())
+depth = float(input())
+
+volume = width * length * depth
+
+time_seconds = volume * 15
+
+time_minutes = time_seconds / 60
+
+print(f"ต้องใช้เวลาในการเติมนำ้ {time_minutes:.2f} นาที.")
